563e64f99cc2bfc3f579c01ce50e78a4df1b0439
devops-tao <471000072@qq.com>
2022-05-06 17:13:45 +0800

    初始化配置完善

--- common/js/config.js
+++ common/js/config.js
@@ -1,12 +1,12 @@
 var config = {
 	// api请求地址
-	baseUrl: '',
+	baseUrl: 'https://shop.youl.cc',
 	// 图片域名
-	imgDomain: '',
+	imgDomain: 'https://shop.youl.cc',
 	// H5端域名
-	h5Domain: '',
+	h5Domain: 'https://shop.youl.cch5',
 	// 腾讯地图key
-	mpKey: '', 
+	mpKey: '5WNBZ-2JYR6-SPUSL-M3WGH-U4KDT-K2FYV', 
 	//客服地址
 	webSocket : '',
 	//本地端主动给服务器ping的时间, 0 则不开启 , 单位秒
